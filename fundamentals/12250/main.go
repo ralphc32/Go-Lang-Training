334f02b41703ff8120027833be7a4f1521991d11
ralphc32 <ralph.cabello@demandscience.com>
2022-01-21 09:28:57 +0800

    Uploaded 12250 solution

--- fundamentals/12250/main.go
+++ fundamentals/12250/main.go
@@ -2,24 +2,35 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-var wordMap = map[string]string{"HELLO": "ENGLISH", "HOLA": "SPANISH", "HALLO": "GERMAN", "BONJOUR": "FRENCH", "CIAO": "ITALIAN", "ZDRAVSTVUJTE": "RUSSIAN"}
+var keywords = map[string]string{
+	"HELLO":        "ENGLISH",
+	"HOLA":         "SPANISH",
+	"HALLO":        "GERMAN",
+	"BONJOUR":      "FRENCH",
+	"CIAO":         "ITALIAN",
+	"ZDRAVSTVUJTE": "RUSSIAN"}
 
 func solve(word string) string {
-	if country, ok := wordMap[word]; ok {
+	if country, ok := keywords[word]; ok {
 		return country
 	}
 	return "UNKNOWN"
 }
 
 func main() {
+	in, _ := os.Open("12250.in")
+	defer in.Close()
+	out, _ := os.Create("12250.out")
+	defer out.Close()
 
 	var word string
 	for i := 1; ; i++ {
-		if fmt.Scan("%s", &word); word == "#" {
+		if fmt.Fscan(in, &word); word == "#" {
 			break
 		}
-		fmt.Printf("Case %d: %s\n", i, solve(word))
+		fmt.Fprintf(out, "Case %d: %s\n", i, solve(word))
 	}
 }
