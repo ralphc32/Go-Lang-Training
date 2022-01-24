866baa1543ca703d2369d0e6d4714647659ed394
ralphc32 <ralph.cabello@demandscience.com>
2022-01-24 11:32:46 +0800

    Uploaded Card Tricks Exercism solution

---
+++ exercism/card-tricks/card_tricks.go
@@ -0,0 +1,49 @@
+package cards
+
+// GetItem retrieves an item from a slice at given position. The second return value indicates whether
+// the given index exists in the slice or not.
+func GetItem(slice []int, index int) (int, bool) {
+	if index < 0 || len(slice) < index+1 {
+		return 0, false
+	} else {
+		if slice[index] < 0 {
+			return 0, false
+		} else {
+			return slice[index], true
+		}
+	}
+}
+
+// SetItem writes an item to a slice at given position overwriting an existing value.
+// If the index is out of range the value needs to be appended.
+func SetItem(slice []int, index, value int) []int {
+	if index < len(slice) && index >= 0 {
+		slice[index] = value
+		return slice
+	} else {
+		slice = append(slice, value)
+		return slice
+	}
+}
+
+// PrefilledSlice creates a slice of given length and prefills it with the given value.
+func PrefilledSlice(value, length int) []int {
+	if length < 0 {
+		return make([]int, 0)
+	}
+	validate := make([]int, length)
+	for i := 0; i < length; i++ {
+		validate[i] = value
+	}
+
+	return validate
+}
+
+// RemoveItem removes an item from a slice by modifying the existing slice.
+func RemoveItem(slice []int, index int) []int {
+	if index < 0 || index > len(slice) {
+		return slice
+	} else {
+		return append(slice[:index], slice[index+1:]...)
+	}
+}
