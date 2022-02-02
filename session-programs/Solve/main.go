de5a5c991c4cc84613e11229daa63da4d7f73de2
ralphc32 <ralph.cabello@demandscience.com>
2022-02-02 13:18:56 +0800

    Implemented basic computation method

---
+++ session-programs/Solve/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/solve", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Fatal(err)
+	}
+	v, ok := r.Form["coef"]
+	fmt.Fprintf(w, "%v %v\n", v, ok)
+	var x, y int
+	if n, _ := fmt.Sscanf(v[0], "%d*%d", &x, &y); n == 2 {
+		fmt.Fprintf(w, "%d * %d = %d\n", x, y, x*y)
+	}
+}
